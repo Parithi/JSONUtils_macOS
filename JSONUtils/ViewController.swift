95bc5735ad888fe0f214b05f31d4a06c6c85c4ff
ElamParithi Arul <elamparithiarul@gmail.com>
2019-12-17 11:35:03 -0500

    First Commit

--- JSONUtils/ViewController.swift
+++ JSONUtils/ViewController.swift
@@ -10,10 +10,32 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet var codeTextView: NSTextView!
+    @IBOutlet weak var actionButton: NSButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        codeTextView.font = NSFont(name: "Monaco", size: 14)
+    }
+    
+    @IBAction func actionClicked(_ sender: Any) {
+        guard codeTextView.string.count > 0 else { return }
+        
+        let string = codeTextView.string
+        
+        codeTextView.string = getConvertedString(string: string)
+        
+    }
+    
+    func getConvertedString(string : String) -> String {
+        let unquotedString = string.replacingOccurrences(of: "\\", with: "")
+        return unquotedString.withoutDoubleQuotes()
+    }
+    
+    
+    override func viewDidAppear() {
+        super.viewDidAppear()
+        self.view.window?.title = "JSON Utils"
     }
 
     override var representedObject: Any? {
@@ -25,3 +47,11 @@ class ViewController: NSViewController {
 
 }
 
+extension String {
+    func withoutDoubleQuotes() -> String {
+        if self.hasPrefix("\""), self.hasSuffix("\""), self.count > 1 {
+            return String(self.dropFirst().dropLast())
+        }
+        return self
+    }
+}
