e90bf41f689194c7acc702deba8912e63eb98239
ElamParithi Arul <elamparithiarul@gmail.com>
2019-12-17 11:04:43 -0500

    Initial Commit

---
+++ JSONUtils/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  JSONUtils
+//
+//  Created by Eyes on 2019-12-17.
+//  Copyright © 2019 Parithi Network. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
